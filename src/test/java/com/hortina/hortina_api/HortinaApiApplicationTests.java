6a9a088abf0ecc8f4668464e563c94dc8633b0e6
Alejandro Vázquez Corral <alex.vazquez.97@gmail.com>
2025-10-25 12:50:28 +0200

    Actualización para añadir propiedades servidor

---
+++ src/test/java/com/hortina/hortina_api/HortinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hortina.hortina_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortinaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
